e12fa9be342cd4a5a9733ee8e1b8d89b4a4114a3
femaldonado <fmaldonado.inacio@gmail.com>
2022-05-23 13:48:32 -0700

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let string = "";
+    if (times < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < times; i++) {
+        string = string + word;
+    }
+    return string;
 };
-
+repeatString("hey, 3") //returns heyheyhey 
 // Do not edit below this line
 module.exports = repeatString;
