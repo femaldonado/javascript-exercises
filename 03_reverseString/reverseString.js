f0a94b262ceae7e7af5a8ed1702e64fb2a0f52d0
femaldonado <fmaldonado.inacio@gmail.com>
2022-05-23 14:48:48 -0700

    Reverse String

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const splitString = str.split("");
+    const reverseArray = splitString.reverse();
+    const joinArray = reverseArray.join("");
+    return joinArray; 
+
+
+
+
 
 };
 
+
+
+
 // Do not edit below this line
 module.exports = reverseString;
+
+
+
+
