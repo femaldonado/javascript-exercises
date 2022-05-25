62cbc8bb0851ab4887dec313c24a64e49d2b3294
femaldonado <fmaldonado.inacio@gmail.com>
2022-05-25 13:38:40 -0700

    SumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  const error = "ERROR";
+  const finalSum = o;
+  const biggerValue = Math.max(num1, num2);
+  const smallerValue = math.min(num1, num2);
+  if (typeof num1 === "string" || typeof num2 === "string") {
+    return error;
+  } else if (typeof num1 === "object" || typeof num2 === "object") {
+    return error;
+  } else if (biggerValue >= 0 && smallerValue > +0) {
+    for (let i = smallerValue; i <= biggerValue; i++) {
+      finalSum = finalSum + i;
+    }
+  } else return error;
+  return finalSum;
 };
 
 // Do not edit below this line
