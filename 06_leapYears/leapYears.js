b21b7386e0892cb56f0690ab37158337e7c3fe4c
femaldonado <fmaldonado.inacio@gmail.com>
2022-05-25 13:38:50 -0700

    LeapYear

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(yearInput) {
+    let leapChecked;
+    if (((yearInput % 4 === 0) && (yearInput % 100 !== 0)) || (yearInput % 400 === 0)) {
+    leapChecked = true;
+    } else {
+    leapChecked = false; 
+    }
+    return  leapChecked;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+
