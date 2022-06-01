f5ff4432967c5a1de2c463036c721f6a99dd0955
femaldonado <fmaldonado.inacio@gmail.com>
2022-06-01 11:29:30 -0700

    Update removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,18 @@
-const removeFromArray = function(...args) {
-    const array = args[0];
-    const newArray = [];
-    array.forEach((item) => {
-        if (!args.includes(item)) {
-            newArray.push(item);
-            console.log(newArray);
-        }
-        });
-        return newArray;
-        };
+// array - [1, 2, 3, 4]
+// valuesToRemove - [3, 2]
+const removeFromArray = function (array, ...valuesToRemove) {
+  const newArray = []; //[1, 4]
 
+  array.forEach((item) => { // 2
+    const isValueRemovable = valuesToRemove.includes(item) // true
+
+    if (!isValueRemovable) { // false
+      newArray.push(item);
+    }
+  });
+
+  return newArray;
+};
 
 // Do not edit below this line
 module.exports = removeFromArray;
